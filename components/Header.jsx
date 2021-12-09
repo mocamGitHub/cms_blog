83509f9e3d411299c75074bf6c46b7b280d93b56
Mo Cam <morris@mocampbell.com>
2021-12-09 00:01:39 -0500

    Graphql Data Fetching

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
                 <div className="hidden md:float-left md:contents">
                     {categories.map((category) => (
                         <Link key={category.slug} href={'/category/${category.slug}'}>
-                            <span className="md:float-rigth mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                            <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                                 {category.name}
                             </span>
                         </Link>
